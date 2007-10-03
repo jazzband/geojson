b0ed864ca6e65ce5543cd6be8bffa33822b3a5e2
seang <seang@b426a367-1105-0410-b9ff-cdf4ab011145>
2007-10-03 18:32:15 +0000

    Incorporate readme into setup's long description
    git-svn-id: http://svn.gispython.org/svn/gispy/GeoJSON/trunk@887 b426a367-1105-0410-b9ff-cdf4ab011145

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 
 from setuptools import setup
 
+# Get text from README.txt
+readme_text = file('README.txt', 'rb').read()
+
 setup(name          = 'GeoJSON',
       version       = '1.0a2',
       description   = 'Encoder/decoder for simple GIS features',
@@ -11,11 +14,11 @@ setup(name          = 'GeoJSON',
       maintainer        = 'Sean Gillies',
       maintainer_email  = 'sgillies@frii.com',
       url           = 'http://trac.gispython.org/projects/PCL/wiki/GeoJSON',
+      long_description = readme_text,
       packages          = ['geojson'],
       install_requires  = ['simplejson', 'setuptools'],
-      tests_require     = ['zope.testing'],
       classifiers   = [
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',

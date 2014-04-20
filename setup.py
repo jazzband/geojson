b1bb6e84412f51a0149ee83ecb44fd5cbed6fe18
Corey Farwell <coreyf@rwell.org>
2014-04-19 21:56:39 -0700

    Bump the version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import multiprocessing  # NOQA
 
 setup(
     name="geojson",
-    version="1.0.6",
+    version="1.0.7",
     description="Python bindings and utilities for GeoJSON",
     license="BSD",
     keywords="gis geography json",

b5a7f3caa488a498dcf65705b3fa966f46b98dba
Corey Farwell <coreyf@rwell.org>
2014-04-23 09:25:00 -0700

    Add 3.4 classifier

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Scientific/Engineering :: GIS",
     ]
 )

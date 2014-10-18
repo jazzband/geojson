fc3a65222b74e1513d6196312403417e9a6f7b1c
Corey Farwell <coreyf@rwell.org>
2014-10-17 21:46:22 -0400

    Fix syntax error

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def test_suite():
     suite.addTest(doctest.DocFileSuite("README.rst"))
     return suite
 
-if sys.version_info[:2] [(2, 6), (2, 7)] and \
+if sys.version_info[:2] not in [(2, 6), (2, 7)] and \
         sys.version_info[:1] not in [(3, )]:
     sys.stderr.write("Sorry, only Python 2.7, and 3.x are supported "
                      "at this time.\n")

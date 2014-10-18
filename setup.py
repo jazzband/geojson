361a220325eea8178ce833252b3d3fdca4d169ce
Corey Farwell <coreyf@rwell.org>
2014-10-17 21:39:00 -0400

    Stop supporting/testing on Python 2.6
    It's no longer updated and should not be used

--- setup.py
+++ setup.py
@@ -18,18 +18,14 @@ def test_suite():
     suite.addTest(doctest.DocFileSuite("README.rst"))
     return suite
 
-if sys.version_info[:2] not in [(2, 6), (2, 7)] and \
+if sys.version_info[:2] [(2, 6), (2, 7)] and \
         sys.version_info[:1] not in [(3, )]:
-    sys.stderr.write("Sorry, only Python 2.6, 2.7, and 3.x are supported "
+    sys.stderr.write("Sorry, only Python 2.7, and 3.x are supported "
                      "at this time.\n")
     exit(1)
 
-tests_require = []
-if sys.version_info[:2] == (2, 6):
-    tests_require.append("unittest2")
-
 # Get around this issue: http://bugs.python.org/issue15881
-# Appears to be a problem in older versions of Python 2.6 and 2.7
+# Appears to be a problem in older versions of Python 2.7
 import multiprocessing  # NOQA
 
 setup(
@@ -49,7 +45,6 @@ setup(
     package_data={"geojson": ["*.rst"]},
     install_requires=["setuptools"],
     test_suite="setup.test_suite",
-    tests_require=tests_require,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -58,7 +53,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",

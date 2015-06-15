04ec9217e7a1a63542f7d463267aa7cf31359592
Sergey Romanov <xxsmotur@gmail.com>
2015-06-15 21:30:58 +0500

    Fix string lines

--- geojson/validation.py
+++ geojson/validation.py
@@ -4,16 +4,16 @@ def IsValid(obj):
     result = {'valid': 'no', 'message':''}
     if isinstance(obj, geojson.LineString):
         if len(obj['coordinates']) < 2:
-            result['message'] = 'the "coordinates" member must be an array \
-                    of two or more positions'
+            result['message'] = 'the "coordinates" member must be an array ' \
+                    'of two or more positions'
             return result
 
     if isinstance(obj, geojson.MultiLineString):
         coord = obj['coordinates']
         if not isinstance(coord, list) or \
                 not all([len(ls) >= 2 for ls in coord]):
-            result['message'] = 'the "coordinates" member must be an array \
-                    of LineString coordinate arrays'
+            result['message'] = 'the "coordinates" member must be an array ' \
+                    'of LineString coordinate arrays'
             return result
     result['valid'] = 'yes'
     return result

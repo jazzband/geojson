877b381d82d0e56ea0764f67ba4f846e9c3d788c
Benny Lichtner <kak0iy0kunai@gmail.com>
2015-06-17 14:53:25 -0700

    Use class variable instead of instance variable

--- geojson/geometry.py
+++ geojson/geometry.py
@@ -1,11 +1,19 @@
 from decimal import Decimal
 
 from geojson.base import GeoJSON
+import sys
+
 
 class Geometry(GeoJSON):
     """
     Represents an abstract base class for a WGS84 geometry.
     """
+
+    if (sys.version_info[0] == 3):
+        # Python 3.x has no long type
+        JSON_compliant_types = (float, int, Decimal)
+    else:
+        JSON_compliant_types = (float, int, Decimal, long)
 
     def __init__(self, coordinates=None, crs=None, **extra):
         """
@@ -22,19 +30,13 @@ class Geometry(GeoJSON):
         self.clean_coordinates(self["coordinates"])
         if crs:
             self["crs"] = self.to_instance(crs, strict=True)
-        
-        if (sys.version_info[0] == 3):
-            # Python 3.x has no long type
-            self.JSON_compliant_types = (float, int, Decimal)
-        else:
-            self.JSON_compliant_types = (float, int, Decimal, long)
 
     @classmethod
     def clean_coordinates(cls, coords):
         for coord in coords:
             if isinstance(coord, (list, tuple)):
                 cls.clean_coordinates(coord)
-            elif not isinstance(coord, self.JSON_compliant_types):
+            elif not isinstance(coord, cls.JSON_compliant_types):
                 raise ValueError("%r is not JSON compliant number" % coord)
 
 

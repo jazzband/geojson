328e8f26666436f1d456d58854ea7cb19f6fb01c
Rowan Winsemius <rowanwins@yahoo.com.au>
2015-07-28 22:18:34 +1000

    Fixed linting errors

--- geojson/utils.py
+++ geojson/utils.py
@@ -56,26 +56,29 @@ def map_coords(func, obj):
     return {'type': obj['type'], 'coordinates': coordinates}
 
 
-def generate_random(featureType, numberFeatures=1, 
-                    numberVertices=3, boundingBox=[-180.0, -90.0, 180.0, 90.0]):
+def generate_random(featureType, numberFeatures=1,
+                    numberVertices=3,
+                    boundingBox=[-180.0, -90.0, 180.0, 90.0]):
     """
     Generates random geojson features depending on the parameters 
     passed through.
 
-    :param featureType: A geometry type 
+    :param featureType: A geometry type
     :type string: Point, LineString, Polygon
-    :param numberFeatures: The number of features that will be returned 
+    :param numberFeatures: The number of features that will be returned
     :type int: defaults to 1
-    :param numberVertices: The number vertices that a linestring or polygon will have  
+    :param numberVertices: The number vertices that 
+    a linestring or polygon will have
     :type int: defaults to 3
-    :param boundingBox: A bounding box in which features will be restricted to 
+    :param boundingBox: A bounding box in which features will be restricted to
     :type string: defaults to the world - [-180.0, -90.0, 180.0, 90.0]
     :return: The resulting random geojson object or geometry collection.
     :rtype: object
     :raises ValueError: if there is no featureType provided.
     """
     from geojson import Point, LineString, Polygon, GeometryCollection
-    import random, math
+    import random
+    import math
 
     lonMin = boundingBox[0]
     lonMax = boundingBox[2]
@@ -98,15 +101,13 @@ def generate_random(featureType, numberFeatures=1,
             coords.append((randomLon(), randomLat()))
         return LineString(coords)
 
-
     def createPoly():
         aveRadius = 60
         ctrX = 0.1
         ctrY = 0.2
-        irregularity = clip( 0.1, 0, 1 ) * 2 * math.pi / numberVertices
-        spikeyness = clip( 0.5, 0, 1 ) * aveRadius
+        irregularity = clip(0.1, 0, 1) * 2 * math.pi / numberVertices
+        spikeyness = clip(0.5, 0, 1) * aveRadius
 
-        # generate n angle steps
         angleSteps = []
         lower = (2 * math.pi / numberVertices) - irregularity
         upper = (2 * math.pi / numberVertices) + irregularity
@@ -116,12 +117,10 @@ def generate_random(featureType, numberFeatures=1,
             angleSteps.append(tmp)
             sum = sum + tmp
 
-        # normalize the steps so that point 0 and point n+1 are the same
         k = sum / (2 * math.pi)
         for i in range(numberVertices):
             angleSteps[i] = angleSteps[i] / k
 
-        # now generate the points
         points = []
         angle = random.uniform(0, 2 * math.pi)
 
@@ -129,18 +128,22 @@ def generate_random(featureType, numberFeatures=1,
             r_i = clip(random.gauss(aveRadius, spikeyness), 0, 2 * aveRadius)
             x = ctrX + r_i*math.cos(angle)
             y = ctrY + r_i*math.sin(angle)
-            points.append( (int(x), int(y)) )
+            points.append((int(x), int(y)))
             angle = angle + angleSteps[i]
 
         firstVal = points[0]
         points.append(firstVal)
         return Polygon([points])
-    
-    def clip(x, min, max) :
-        if( min > max ) :  return x 
-        elif( x < min ) :  return min
-        elif( x > max ) :  return max
-        else : return x
+
+    def clip(x, min, max):
+        if(min > max):
+            return x 
+        elif(x < min):
+            return min
+        elif(x > max):
+            return max
+        else:
+            return x
 
     if numberFeatures > 1:
         group = []

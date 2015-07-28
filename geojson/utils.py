3e7c5e7817684f3e99efb6a7df74f01feb8abba2
Rowan Winsemius <rowanwins@yahoo.com.au>
2015-07-28 22:05:24 +1000

    Fixed indentation

--- geojson/utils.py
+++ geojson/utils.py
@@ -57,109 +57,109 @@ def map_coords(func, obj):
 
 
 def generate_random(featureType, numberFeatures=1, 
-					numberVertices=3, boundingBox=[-180.0, -90.0, 180.0, 90.0]):
-	"""
-	Generates random geojson features depending on the parameters 
-	passed through.
+                    numberVertices=3, boundingBox=[-180.0, -90.0, 180.0, 90.0]):
+    """
+    Generates random geojson features depending on the parameters 
+    passed through.
 
-	:param featureType: A geometry type 
-	:type string: Point, LineString, Polygon
-	:param numberFeatures: The number of features that will be returned 
-	:type int: defaults to 1
-	:param numberVertices: The number vertices that a linestring or polygon will have  
-	:type int: defaults to 3
-	:param boundingBox: A bounding box in which features will be restricted to 
-	:type string: defaults to the world - [-180.0, -90.0, 180.0, 90.0]
-	:return: The resulting random geojson object or geometry collection.
-	:rtype: object
-	:raises ValueError: if there is no featureType provided.
-	"""
-	from geojson import Point, LineString, Polygon, GeometryCollection
-	import random, math
+    :param featureType: A geometry type 
+    :type string: Point, LineString, Polygon
+    :param numberFeatures: The number of features that will be returned 
+    :type int: defaults to 1
+    :param numberVertices: The number vertices that a linestring or polygon will have  
+    :type int: defaults to 3
+    :param boundingBox: A bounding box in which features will be restricted to 
+    :type string: defaults to the world - [-180.0, -90.0, 180.0, 90.0]
+    :return: The resulting random geojson object or geometry collection.
+    :rtype: object
+    :raises ValueError: if there is no featureType provided.
+    """
+    from geojson import Point, LineString, Polygon, GeometryCollection
+    import random, math
 
-	lonMin = boundingBox[0]
-	lonMax = boundingBox[2]
+    lonMin = boundingBox[0]
+    lonMax = boundingBox[2]
 
-	def randomLon():
-		return random.randrange(lonMin, lonMax)
+    def randomLon():
+        return random.randrange(lonMin, lonMax)
 
-	latMin = boundingBox[1]
-	latMax = boundingBox[3]
+    latMin = boundingBox[1]
+    latMax = boundingBox[3]
 
-	def randomLat():
-		return random.randrange(latMin, latMax)
+    def randomLat():
+        return random.randrange(latMin, latMax)
 
-	def createPoint():
-		return Point((randomLon(), randomLat()))
+    def createPoint():
+        return Point((randomLon(), randomLat()))
 
-	def createLine():
-		coords = []
-		for i in range(0, numberVertices):
-			coords.append((randomLon(), randomLat()))
-		return LineString(coords)
+    def createLine():
+        coords = []
+        for i in range(0, numberVertices):
+            coords.append((randomLon(), randomLat()))
+        return LineString(coords)
 
 
-	def createPoly():
-		aveRadius = 60
-		ctrX = 0.1
-		ctrY = 0.2
-		irregularity = clip( 0.1, 0, 1 ) * 2 * math.pi / numberVertices
-		spikeyness = clip( 0.5, 0, 1 ) * aveRadius
+    def createPoly():
+        aveRadius = 60
+        ctrX = 0.1
+        ctrY = 0.2
+        irregularity = clip( 0.1, 0, 1 ) * 2 * math.pi / numberVertices
+        spikeyness = clip( 0.5, 0, 1 ) * aveRadius
 
-		# generate n angle steps
-		angleSteps = []
-		lower = (2 * math.pi / numberVertices) - irregularity
-		upper = (2 * math.pi / numberVertices) + irregularity
-		sum = 0
-		for i in range(numberVertices):
-			tmp = random.uniform(lower, upper)
-			angleSteps.append(tmp)
-			sum = sum + tmp
+        # generate n angle steps
+        angleSteps = []
+        lower = (2 * math.pi / numberVertices) - irregularity
+        upper = (2 * math.pi / numberVertices) + irregularity
+        sum = 0
+        for i in range(numberVertices):
+            tmp = random.uniform(lower, upper)
+            angleSteps.append(tmp)
+            sum = sum + tmp
 
-		# normalize the steps so that point 0 and point n+1 are the same
-		k = sum / (2 * math.pi)
-		for i in range(numberVertices):
-			angleSteps[i] = angleSteps[i] / k
+        # normalize the steps so that point 0 and point n+1 are the same
+        k = sum / (2 * math.pi)
+        for i in range(numberVertices):
+            angleSteps[i] = angleSteps[i] / k
 
-		# now generate the points
-		points = []
-		angle = random.uniform(0, 2 * math.pi)
+        # now generate the points
+        points = []
+        angle = random.uniform(0, 2 * math.pi)
 
-		for i in range(numberVertices):
-			r_i = clip(random.gauss(aveRadius, spikeyness), 0, 2 * aveRadius)
-			x = ctrX + r_i*math.cos(angle)
-			y = ctrY + r_i*math.sin(angle)
-			points.append( (int(x), int(y)) )
-			angle = angle + angleSteps[i]
+        for i in range(numberVertices):
+            r_i = clip(random.gauss(aveRadius, spikeyness), 0, 2 * aveRadius)
+            x = ctrX + r_i*math.cos(angle)
+            y = ctrY + r_i*math.sin(angle)
+            points.append( (int(x), int(y)) )
+            angle = angle + angleSteps[i]
 
-		firstVal = points[0]
-		points.append(firstVal)
-		return Polygon([points])
-	
-	def clip(x, min, max) :
-		if( min > max ) :  return x 
-		elif( x < min ) :  return min
-		elif( x > max ) :  return max
-		else : return x
+        firstVal = points[0]
+        points.append(firstVal)
+        return Polygon([points])
+    
+    def clip(x, min, max) :
+        if( min > max ) :  return x 
+        elif( x < min ) :  return min
+        elif( x > max ) :  return max
+        else : return x
 
-	if numberFeatures > 1:
-		group = []
-		for i in range(0, numberFeatures):
-			if featureType == 'Point':
-				group.append(createPoint())
-			elif featureType == 'LineString':
-				group.append(createLine())
-			elif featureType == 'Polygon':
-				group.append(createPoly())
+    if numberFeatures > 1:
+        group = []
+        for i in range(0, numberFeatures):
+            if featureType == 'Point':
+                group.append(createPoint())
+            elif featureType == 'LineString':
+                group.append(createLine())
+            elif featureType == 'Polygon':
+                group.append(createPoly())
 
-		return GeometryCollection(group)
+        return GeometryCollection(group)
 
-	else:
-		if featureType == 'Point':
-			return createPoint()
+    else:
+        if featureType == 'Point':
+            return createPoint()
 
-		if featureType == 'LineString':
-			return createLine()
+        if featureType == 'LineString':
+            return createLine()
 
-		if featureType == 'Polygon':
-			return createPoly()
+        if featureType == 'Polygon':
+            return createPoly()

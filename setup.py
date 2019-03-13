ce076e624c450169d8ab020d90ef595ea99963f5
Kamil Gałuszka <kamil.galuszka@solution4future.com>
2019-03-13 17:43:37 +0100

    feat: drop support for Python 3.3 and add support for 3.7

--- setup.py
+++ setup.py
@@ -66,10 +66,10 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering :: GIS",
     ]
 )

434a9dcd42f9e226676570e3b1182574062732a7
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-08-08 11:40:04 +0300

    Test Python 3.10 release candidate

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ def test_suite():
 
 
 major_version, minor_version = sys.version_info[:2]
-if not (major_version == 3 and 6 <= minor_version <= 9):
-    sys.stderr.write("Sorry, only Python 3.6 - 3.9 are "
+if not (major_version == 3 and 6 <= minor_version <= 10):
+    sys.stderr.write("Sorry, only Python 3.6 - 3.10 are "
                      "supported at this time.\n")
     exit(1)
 

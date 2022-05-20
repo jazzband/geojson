8bac26e4b2f44e22d11bd0f79dbb2d69dfc245a9
r <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGZdL5+4M4rRsztN84keAE4KrYNVu9hWoIN5ourxmZuF 7869818+rayrrr@users.noreply.github.com>
2022-05-20 10:01:40 -0400

    3.10.4 > 3.10 stringwise; adjust conditional accordingly

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def test_suite():
 
 
 major_version, minor_version = sys.version_info[:2]
-if not (major_version == 3 and 6 <= minor_version <= 10):
+if not (major_version == 3 and 6 <= minor_version < 11):
     sys.stderr.write("Sorry, only Python 3.6 - 3.10 are "
                      "supported at this time.\n")
     exit(1)
@@ -48,7 +48,7 @@ setup(
     package_data={"geojson": ["*.rst"]},
     install_requires=[],
     test_suite="setup.test_suite",
-    python_requires=">=3.6, <=3.10",
+    python_requires=">=3.6, <3.11",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",

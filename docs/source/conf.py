fde6791aae3beab2b8c830f3d26129b9adee97f1
Ray <7869818+rayrrr@users.noreply.github.com>
2024-08-06 21:10:56 -0400

    RtD fix

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'geojson'
+copyright = '2024, Sean Gillies, Matthew Russell, Corey Farwell, Blake Grotewold, Zsolt Ero, Sergey Romaov, Ray Riga'
+author = 'Sean Gillies, Matthew Russell, Corey Farwell, Blake Grotewold, Zsolt Ero, Sergey Romaov, Ray Riga'
+release = '3.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,10 @@
+.. geojson documentation master file, created by
+   sphinx-quickstart on Tue Aug  6 21:00:20 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. include:: ../../README.rst

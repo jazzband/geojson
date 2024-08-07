8f4a473aaa45e71323a1c3df030a58ce5fa5d8be
Ray <7869818+rayrrr@users.noreply.github.com>
2024-08-06 21:20:41 -0400

    Use RtD theme for Sphinx docs
    Started with `sphinx-quickstart` output then changed it back to the theme we are already using.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,10 +19,9 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ['_theme']
 html_static_path = ['_static']
